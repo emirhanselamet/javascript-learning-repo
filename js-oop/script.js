83c8e911c7b8066f7efe0d805a82826e4707e008
Emirhan <63867996+emirhanincodes@users.noreply.github.com>
2021-05-10 10:32:22 +0300

    new lesson

---
+++ js-oop/script.js
@@ -0,0 +1,40 @@
+// object literals
+
+// let yigit = {
+//     name:'emirhan',
+//     yearOfBirth : 2005,
+//     job :'student'
+// }
+
+// function Person(name,yearOfBirth,job){
+//     this.name = name;
+//     this.yearOfBirth=yearOfBirth;
+//     this.job=job;
+//     this.calculateAge = function(){
+//         return 2021 - this.yearOfBirth;
+//     }
+// }
+
+let Person = function(name,yearOfBirth,job){
+    this.name = name;
+    this.yearOfBirth=yearOfBirth;
+    this.job=job;
+    this.calculateAge = function(){
+        return 2021 - this.yearOfBirth;
+    }
+}
+
+let emirhan = new Person('emirhan',2005,'student');
+let teacher = new Person('teacher',1989,'teacher');
+
+console.log(yigit.name);
+console.log(yigit.yearOfBirth);
+console.log(yigit.job);
+console.log(yigit.calculateAge());
+
+console.log('***************');
+
+console.log(teacher.name);
+console.log(teacher.yearOfBirth);
+console.log(teacher.job);
+console.log(teacher.calculateAge());
