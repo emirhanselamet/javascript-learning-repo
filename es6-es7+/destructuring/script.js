3938578d4e61e746256c84d563b9fec35db35e28
Emirhan <63867996+emirhanincodes@users.noreply.github.com>
2021-05-15 23:19:06 +0300

    Update script.js

--- es6-es7+/destructuring/script.js
+++ es6-es7+/destructuring/script.js
@@ -1,14 +1,25 @@
 //destructring
-var a,b,rest;
-[a,b] = [10,20];
+var a, b, rest;
+[a, b] = [10, 20];
 console.log(a);
 console.log(b);
 
-[a,b,...rest] = [10,20,30,40,50];
+[a, b, ...rest] = [10, 20, 30, 40, 50];
 console.log(a);
 console.log(b);
 console.log(rest);
 
-//destructring assignment
+
 // array destructring
+
+const [server, port, timeout] = arrConfig;
+console.log(server, port, timeout);
+
 //object destructring
+const objConfig = {
+    server:'localhost',
+    port: ' 8080',
+    timeout:900
+};
+const [server, port, timeout] = objConfig;
+console.log(server,port,timeout)
