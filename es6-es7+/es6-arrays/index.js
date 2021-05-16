0d2a281e13f7877180fe722eed5b24675da9bafd
Emirhan <63867996+emirhanincodes@users.noreply.github.com>
2021-05-16 23:48:50 +0300

    Update index.js

--- es6-es7+/es6-arrays/index.js
+++ es6-es7+/es6-arrays/index.js
@@ -1,8 +1,10 @@
 const products = [
     {name:'s7',price:3000},
     {name:'s8',price:2000},
-    {name:'s9',price:13000}
+    {name:'s9',price:13000},
+    {name:'s10',price:13000}
 ]
 console.log(Array.from(products,prd => prd.name='s8'));
 console.log(Array.find(products,prd => prd.name='s8'));
 console.log(Array.filter(products,prd => prd.name='s8'));
+console.log(products.findIndex(prd => prd.price==2000));
