03b9da26f2c5923d638f159d99b7c5ce1656bfdc
Emirhan <63867996+emirhanincodes@users.noreply.github.com>
2021-05-16 23:47:42 +0300

    Update index.js

--- es6-es7+/es6-arrays/index.js
+++ es6-es7+/es6-arrays/index.js
@@ -4,3 +4,5 @@ const products = [
     {name:'s9',price:13000}
 ]
 console.log(Array.from(products,prd => prd.name='s8'));
+console.log(Array.find(products,prd => prd.name='s8'));
+console.log(Array.filter(products,prd => prd.name='s8'));
