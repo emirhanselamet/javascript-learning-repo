8fd64ba14c87491092cc241327d3b77a20eae059
Emirhan <63867996+emirhanincodes@users.noreply.github.com>
2021-05-19 23:40:48 +0300

    restapi

---
+++ restfulapi-ajax/restapi-httpget/index.js
@@ -0,0 +1,66 @@
+document.querySelector('#getOne').addEventListener('click',getOne);
+document.querySelector('#getAll').addEventListener('click',getAll);
+
+function getOne() {  
+    var id = document.getElementById('postID').value;
+    var url = "https://jsonplaceholder.typicode.com/posts/"+id
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET',url,true);
+    xhr.onload =function () {  
+        if (this.status === 200) {
+          var post  = JSON.parse(this.responseText);
+          var html ="";
+
+
+                html +=`<div class="card mb-2 mt-2">
+                <div class="card-header">
+                ${post.id}-${post.title}
+                </div>
+                <div class="card-body">
+                  <blockquote class="blockquote mb-0">
+                    <p>${post.body}</p>
+                    <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                  </blockquote>
+                </div>
+              </div>`
+        
+
+
+         
+          document.querySelector('#results').innerHTML = html;
+        }
+    }
+    xhr.send();
+}
+
+function getAll() {  
+    var url = "https://jsonplaceholder.typicode.com/posts"
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET',url,true);
+    xhr.onload =function () {  
+        if (this.status === 200) {
+          var posts  = JSON.parse(this.responseText);
+          var html ="";
+
+            posts.forEach(post => {
+                html +=`<div class="card mb-2 mt-2">
+                <div class="card-header">
+                  ${post.title}
+                </div>
+                <div class="card-body">
+                  <blockquote class="blockquote mb-0">
+                    <p>${post.body}</p>
+                    <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                  </blockquote>
+                </div>
+              </div>`
+            });
+
+
+         
+          document.querySelector('#results').innerHTML = html;
+        }
+    }
+    xhr.send();
+}
+
