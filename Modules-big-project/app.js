21dd60fbe1adc98fa018c5905ca3657f2a4d41fc
Emirhan <63867996+emirhanincodes@users.noreply.github.com>
2021-05-24 23:38:37 +0300

    update

--- Modules-big-project/app.js
+++ Modules-big-project/app.js
@@ -1,3 +1,5 @@
+const { UI } = require("winjs");
+
 //Storage Controller
 const StorageContoller = (function () {
 
@@ -13,11 +15,7 @@ const ProductContoller = (function () {
     }
 
     const data = {
-        products: [
-            { id: 0, name: 'Monitor', price: 100 },
-            { id: 1, name: 'Ram', price: 200 },
-            { id: 2, name: 'Klavte', price: 300 }
-        ],
+        products: [],
         selectedProduct: null,
         totalPrice: 0
     }
@@ -27,6 +25,17 @@ const ProductContoller = (function () {
         },
         getData: function () {
             return data;
+        },
+        addProduct: function (name, pric) {
+            let id;
+            if (data.items.length > 0) {
+                id = data.items[data.prodcuts.length - 1].id + 1;
+            } else {
+                id = 0;
+            }
+            const newProduct = new Product(id, name, parseFloat(price));
+            data.items.push(newProduct);
+            return newProduct;
         }
     }
 })();
@@ -38,7 +47,11 @@ const ProductContoller = (function () {
 const UIContoller = (function () {
 
     const Selectors = {
-        productList: "#itemlist"
+        productList: "#itemlist",
+        addButton: '.addBtn',
+        productName: '#productName',
+        productPrice: '#productPrice',
+        productCard:'#productCard'
     }
 
 
@@ -62,18 +75,66 @@ const UIContoller = (function () {
         },
         getSelectors: function () {
             return Selectors;
+        },
+        addProduct: function (prd) {
+            document.querySelector(Selectors.productCard).style.display='block';
+            var item = `
+            <tr>
+            <td>${prd.id}</td>
+            <td>${prd.name}</td>
+            <td>${prd.price}</td>
+            <td class="text-right"><button type="submit" class="btn btn-secondary btn-sm"> <i class="far fa-edit"></i></button></td>
+            </tr>
+            `;
+            document.querySelector(Selectors.productList).innerHTML += item;
+        },
+        clearInput : function () {  
+            document.querySelector(Selectors.productName).value = '';
+            document.querySelector(Selectors.productPrice).value = '';
+        },
+        hideCard : function () {  
+            document.querySelector(Selectors.productCard).style.display='none';
         }
     }
 })();
 //APP Controller
 const App = (function (ProductCtrl, UICtrl) {
+    const UISelectors = UIContoller.getSelectors();
+    //Load Event Listener
+    const loadEventListeners = function () {
+        //add product event
+        document.querySelector(UISelectors.addButton).addEventListener('click', ProductAddSubmit);
+    }
+    const ProductAddSubmit = function (e) {
 
+        const productName = document.querySelector(UISelectors.productName).value;
+        const productPrice = document.querySelector(UISelectors.productPrice).value;
+
+        if (productName !== '' && productPrice !== '') {
+            //add product
+            const newProduct = ProductCtrl.addProduct(productName, productPrice);
+            //add item list
+            UIContoller.addProduct(newProduct)
+
+            UIContoller.clearInputs();
+        }
+
+        console.log(productName, productPrice);
+
+        e.preventDefault();
+    }
     return {
         init: function () {
             console.log('starting app');
-            const prodcuts = ProductCtrl.getProducts();
 
-            UICtrl.createProductList(products);
+            const prodcuts = ProductCtrl.getProducts();
+            if (products.lenghts==0) {
+                UICtrl.hideCard();
+            }else{
+                UICtrl.createProductList(products)
+            }
+            
+            loadEventListeners();
         }
     }
 
